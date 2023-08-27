0d4fa6d103e70beaeaa3c3df658e3a018504a749
Eduardo Martini <123970080+duudumartini@users.noreply.github.com>
2023-08-27 13:24:57 -0300

    Corrigido bug do joystick

--- Prato Perfeito/Assets/Scenes/ScriptsGame/ControlaJogador.cs
+++ Prato Perfeito/Assets/Scenes/ScriptsGame/ControlaJogador.cs
@@ -14,7 +14,8 @@ public class ControlaJogador : MonoBehaviour
     public FixedJoystick joystick;
     public GameObject CanvasCozinha;
     private Vector3 direcao;
-
+    private int VelocidadeMovimento = 5;
+    private int VelocidadeRotacao = 10;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -26,11 +27,27 @@ public class ControlaJogador : MonoBehaviour
         AtivaAnimacao();
     }
 
+    private void FixedUpdate()
+    {
+        float HorizontalInput = joystick.Vertical *-1;
+        float VerticalInput = joystick.Horizontal;
+        Vector3 moveDirection = new Vector3(HorizontalInput, 0.0f, VerticalInput);
+
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, VelocidadeRotacao * Time.deltaTime);
+
+            moveDirection.Normalize();
+        }
+
+        rb.velocity = moveDirection * VelocidadeMovimento;
+    }
+
     private void AtivaAnimacao()
     {
         GetComponent<Animator>().SetFloat("Andar", rb.velocity.magnitude);
+        Debug.Log(rb.velocity.magnitude);
     }
-
-
 
 }
